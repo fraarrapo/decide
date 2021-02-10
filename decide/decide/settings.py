afeabc87471475cda2ac1887efc2bb3b016a0a8e
fraarrapo <fraarrapo@alum.us.es>
2021-02-10 18:27:10 +0100

    Yea

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,8 +70,6 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -161,9 +159,13 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+APIS = {}
+
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
+
+BASEURL = 'https://examen10-02-21.herokuapp.com'
 
 try:
     from local_settings import *
@@ -180,3 +182,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
